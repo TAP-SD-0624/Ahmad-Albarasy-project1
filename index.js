1698180bbbc8f506f7c567d25396c57849254001
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-06-23 23:23:21 +0300

    Fixed some bugs and implemented favorite popup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const themeButton = document.getElementById('themeButton');
-
+const favoritesButton = document.getElementById('favoritesButton');
+const favoritesSection = document.getElementById('favoritesSection');
 themeButton.addEventListener('click', () => {
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
@@ -8,5 +9,14 @@ themeButton.addEventListener('click', () => {
     } else {
         body.classList.remove("light");
         body.classList.add("dark");
+    }
+});
+
+favoritesButton.addEventListener('click', () => {
+    if (favoritesSection.style.display === 'none') {
+        favoritesSection.style.display = 'block';
+    }
+    else {
+        favoritesSection.style.display = 'none';
     }
 });
